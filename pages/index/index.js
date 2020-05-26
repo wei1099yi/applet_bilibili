48384c4b36c2aa81495838e7efc4f7d4041e8e1f
韦荣艺 <1099393790@qq.com>
2020-05-26 18:10:04 +0800

    完成轮播图与导航

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,38 +1,56 @@
 //Page Object
 Page({
   data: {
-    
+    // 首页的导航数据
+    navList: [],
+    // 轮播图数据
+    swiperList: [],
+    // 被点击的首页导航的索引
+    currentIndexNav: 0
   },
   //options(Object)
-  onLoad: function(options){
-    
+  onLoad: function (options) {
+    this.getNavList();
+    this.getSwiperList();
   },
-  onReady: function(){
-    
-  },
-  onShow: function(){
-    
-  },
-  onHide: function(){
 
+  /**
+   * 获取首页导航数据
+   */
+  getNavList() {
+    wx.request({
+      url: 'http://mock-api.com/mnEe4VnJ.mock/navList',
+      success: (result) => {
+        const {navList} = result.data.data;
+        this.setData({
+          navList
+        })
+      }
+    });
   },
-  onUnload: function(){
 
+  /**
+   * 获取轮播图数据
+   */
+  getSwiperList() {
+    wx.request({
+      url: 'http://mock-api.com/mnEe4VnJ.mock/swiperList',
+      success: (result) => {
+        const {swiperList} = result.data.data;
+        this.setData({
+          swiperList
+        })
+      }
+    });
   },
-  onPullDownRefresh: function(){
 
-  },
-  onReachBottom: function(){
-
-  },
-  onShareAppMessage: function(){
-
-  },
-  onPageScroll: function(){
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item){
-
+  /**
+   * 导航点击事件
+   */
+  activeNav(e) {
+    const {index} = e.currentTarget.dataset;
+    this.setData({
+      currentIndexNav: index
+    })
   }
 });
